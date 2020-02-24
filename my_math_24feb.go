b69062d83149941e8bda160bd9438ba3c05827cf
unknown <kamalsir@ymail.com>
2020-02-24 11:52:52 +0530

    1.0

---
+++ my_math_24feb.go
@@ -0,0 +1,10 @@
+package my_math_24feb
+import "fmt"
+func Square(num int) {
+res := num * num
+fmt.Println("square = ", res)
+}
+func Cube(num int) {
+res := num * num * num
+fmt.Println("cube = ", res)
+}
